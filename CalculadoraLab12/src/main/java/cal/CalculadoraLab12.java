ec5a9352b00f2eed23f1e6816de8510c103898ea
cristofer2828 <142689725+cristofer2828@users.noreply.github.com>
2023-11-07 01:28:30 -0500

    Add files via upload

---
+++ CalculadoraLab12/src/main/java/cal/CalculadoraLab12.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package cal;
+
+/**
+ *
+ * @author joshi
+ */
+public class CalculadoraLab12 {
+
+    public static void main(String[] args) {
+        FrameCalculadora Ventana =new FrameCalculadora();
+        Ventana.setLocationRelativeTo(null);
+        Ventana.setResizable(true);
+        Ventana.setVisible(true);
+    }
+}
